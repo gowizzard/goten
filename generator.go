3a0b32105c012bb5e048a05b1104840922f4cb8e
Jonas Kwiedor <info@jj-ideenschmiede.de>
2020-12-30 21:42:21 +0100

    First version
    Add function to create a random token or password

---
+++ generator.go
@@ -0,0 +1,37 @@
+package token
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Characters for the token
+const char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOFQRSTUVWXYZ1234567890"
+
+func init() {
+
+	// Generate random seed, that the key is random
+	// The seed based on the current time with nano seconds
+	rand.Seed(time.Now().UnixNano())
+
+}
+
+// Generate a new funktion
+func New(length int) string {
+
+	// Variable for the user token
+	var token string
+
+	// Range for token
+	for i := 0; i < length; i++ {
+
+		// Building an string
+		token = fmt.Sprintf("%s%s", token, string(char[rand.Intn(len(char))]))
+
+	}
+
+	// Return the token
+	return token
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jojojojonas/token
+
+go 1.15
