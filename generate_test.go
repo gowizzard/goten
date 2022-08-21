9945d9527aeae30216c85f3ec1eff12bbf1d4eda
Jonas Kwiedor <mail@gowizzard.de>
2022-08-21 10:36:32 +0200

    test: Add table driven tests.

--- generate_test.go
+++ generate_test.go
@@ -1,19 +1,58 @@
-package goten
+package goten_test
 
 import (
-	"log"
+	"github.com/gowizzard/goten"
+	"reflect"
 	"testing"
 )
 
-// TestGenerate is to test the generate function and create a token
+// TestGenerate is to test the generate function
+// We simulate the generation and check the result
 func TestGenerate(t *testing.T) {
 
-	options := Options{
-		Numbers: true,
-		Symbols: false,
+	tests := []struct {
+		length  int
+		options goten.Options
+	}{
+		{
+			length: 50,
+			options: goten.Options{
+				Numbers: false,
+				Symbols: false,
+			},
+		},
+		{
+			length: 256,
+			options: goten.Options{
+				Numbers: true,
+				Symbols: false,
+			},
+		},
+		{
+			length: 10,
+			options: goten.Options{
+				Numbers: false,
+				Symbols: true,
+			},
+		},
+		{
+			length: 25,
+			options: goten.Options{
+				Numbers: true,
+				Symbols: true,
+			},
+		},
 	}
 
-	token := Generate(50, &options)
-	log.Println(token)
+	for _, value := range tests {
+
+		token := goten.Generate(value.length, &value.options)
+		length := reflect.ValueOf(token).Len()
+
+		if !reflect.DeepEqual(value.length, length) {
+			t.Fatalf("expected: %d, got %d", value.length, length)
+		}
+
+	}
 
 }
